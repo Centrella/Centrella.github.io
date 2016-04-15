dcaaf2aff386173cbf16f7361c2811d1ed13c03d
johnquiwa <jcq211@nyu.edu>
2016-04-15 10:22:49 -0400

    update photos, redirect straight to videos page

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('app', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.otherwise('/');
+	$urlRouterProvider.otherwise('/landing/videos');
 
     $stateProvider.state('home', {
         url: '/',
